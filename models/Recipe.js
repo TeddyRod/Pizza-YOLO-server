dd7198b1c86ad36261aaad72b04a161a65956459
Teddy ROGERS <teddy.rogers.pro@outlook.com>
2020-06-28 22:39:32 +0200

    models
    creating User Recipe, Ingredient models

---
+++ models/Recipe.js
@@ -0,0 +1,14 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const recipeSchema = new Schema ({
+        name: {type: String, required: true},
+        image: {type: String, required: true},
+        price: {type: Number, required: true},
+        offer : {type: Number, default: null},
+        star: {type: Boolean, default: false},
+        new: {type: Boolean, default: false},
+})
+
+const Recipe = mongoose.model ("Recipe",recipeSchema);
+module.exports = Recipe; 
--- models/User.js
+++ models/User.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  email: {type: String, required: true,},
+  password: {type: String,required: true,},
+  role: {type: String, required: true, enum: ["user", "admin"], default: "user",},
 });
 
 const User = mongoose.model("User", userSchema);
