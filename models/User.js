ed7aaf09ad962609259cdaa1c5433c7b004c36b7
Teddy ROGERS <teddy.rogers.pro@outlook.com>
2020-06-29 15:06:39 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -6,6 +6,7 @@ const userSchema = new Schema({
   password: {type: String,required: true,},
   role: {type: String, required: true, enum: ["user", "admin"], default: "user",},
   fav: [{type: Schema.Types.ObjectId, ref: 'Recipe', default: null}],
+  history: [{type: Schema.Types.ObjectId, ref: 'Recipe', default: null}],
 });
 
 const User = mongoose.model("User", userSchema);
